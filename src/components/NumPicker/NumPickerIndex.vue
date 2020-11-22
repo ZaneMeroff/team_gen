f49310109c96d9d0f9e5680faa324e8be245bd5b
ZaneMeroff <zanemeroff@gmail.com>
2020-11-22 12:41:06 -0700

    Add import for NumPickerNuDisplay in NumPickerIndex

--- src/components/NumPicker/NumPickerIndex.vue
+++ src/components/NumPicker/NumPickerIndex.vue
@@ -1,14 +1,29 @@
 <template>
-  <h1>num picker index</h1>
+  <section class="npi-perimeter-container">
+
+    <NumPickerNumDisplay />
+
+  </section>
 </template>
 
 <script>
-export default {
-  name: "NumPickerIndex",
-  props: {},
-}
+  import NumPickerNumDisplay from "./NumPickerNumDisplay"
+
+  export default {
+    name: "NumPickerIndex",
+    props: {},
+    components: {
+      NumPickerNumDisplay,
+    }
+  }
 </script>
 
 <style scoped>
+
+  .npi-perimeter-container {
+    height: 150px;
+    width: 150px;
+    border: 1px solid #000;
+  }
 
 </style>
