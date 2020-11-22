cb0a836675109a7a73cd3d71adb8f8dba27fa306
ZaneMeroff <zanemeroff@gmail.com>
2020-11-22 12:59:00 -0700

    Add arrow np-arrow-btn class with style

--- src/components/NumPicker/NumPickerArrowBtn.vue
+++ src/components/NumPicker/NumPickerArrowBtn.vue
@@ -1,14 +1,35 @@
 <template>
-  <h1>num picker arrow button</h1>
+  <section class="np-arrow-btn">
+
+    <p>&#8593</p>
+
+  </section>
 </template>
 
 <script>
 export default {
   name: "NumPickerArrowBtn",
   props: {},
+  components: {},
+  data() {
+    return {
+
+    }
+  },
+  methods: {},
+  computed: {},
+  watch: {},
 }
 </script>
 
 <style scoped>
+
+  .np-arrow-btn {
+    height: 75px;
+    width: 75px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 
 </style>
