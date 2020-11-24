2f488416fc9b57f5ca1a7943d8d64ae428ffe53e
ZaneMeroff <zanemeroff@gmail.com>
2020-11-24 13:40:47 -0700

    Create state and mutations for teamNum and playerNum

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,23 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue"
+import Vuex from "vuex"
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    teamNum: 0,
+    playerNum: 0,
   },
+
   mutations: {
+
+    plusteamNum: (state) => state.teamNum++,
+    minusteamNum: (state) => state.teamNum--,
+
+    plusplayerNum: (state) => state.playerNum++,
+    minusplayerNum: (state) => state.playerNum--,
   },
-  actions: {
-  },
-  modules: {
-  }
+
+  actions: {},
+  modules: {},
 })
