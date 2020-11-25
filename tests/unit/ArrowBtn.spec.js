e904839e7d0506a755fd08c4b909b9cb563e815a
ZaneMeroff <zanemeroff@gmail.com>
2020-11-25 14:59:27 -0700

    Add ArrowBtn spec

---
+++ tests/unit/ArrowBtn.spec.js
@@ -0,0 +1,126 @@
+import { shallowMount } from '@vue/test-utils'
+
+import ArrowBtn from "@/components/ArrowBtn"
+
+describe("ArrowBtn", () => {
+
+  describe("snapshots", () => {
+
+    it("should render with default props", () => {
+      const component = shallowMount(ArrowBtn)
+
+      expect(component.element).toMatchSnapshot()
+    })
+  })
+
+  describe("props", () => {
+
+    describe("arrowDirection", () => {
+
+      it("should have a default value", () => {
+        const component = shallowMount(ArrowBtn)
+
+        expect(component.vm.arrowDirection).toEqual("up")
+      })
+
+      it("should accept a string", () => {
+        const propsData = { arrowDirection: "down" }
+        const component = shallowMount(ArrowBtn, { propsData })
+
+        expect(component.vm.arrowDirection).toEqual(propsData.arrowDirection)
+      })
+    })
+  })
+
+  describe("data", () => {
+
+    describe("arrowDisplay", () => {
+
+      it("should have a default value", () => {
+        const component = shallowMount(ArrowBtn)
+
+        expect(component.vm.$data.arrowDisplay).toEqual("&#8593")
+      })
+    })
+
+    describe("arrowKey", () => {
+
+      it("should have a default value", () => {
+        const component = shallowMount(ArrowBtn)
+
+        const expected = {
+          up:    "&#8593",
+          down:  "&#8595",
+          left:  "&#x2190",
+          right: "&#x2192",
+        }
+        expect(component.vm.$data.arrowKey).toEqual(expected)
+      })
+    })
+  })
+
+  describe("methods", () => {
+
+    describe("setArrowDisplay", () => {
+
+      it("should set arrowDisplay if arrowDirection is 'up'", () => {
+        const component = shallowMount(ArrowBtn)
+
+        component.vm.setArrowDisplay("up")
+
+        expect(component.vm.$data.arrowDisplay).toEqual("&#8593")
+      })
+
+      it("should set arrowDisplay if arrowDirection is 'down'", () => {
+        const component = shallowMount(ArrowBtn)
+
+        component.vm.setArrowDisplay("down")
+
+        expect(component.vm.$data.arrowDisplay).toEqual("&#8595")
+      })
+
+      it("should set arrowDisplay if arrowDirection is 'left'", () => {
+        const component = shallowMount(ArrowBtn)
+
+        component.vm.setArrowDisplay("left")
+
+        expect(component.vm.$data.arrowDisplay).toEqual("&#x2190")
+      })
+
+      it("should set arrowDisplay if arrowDirection is 'right'", () => {
+        const component = shallowMount(ArrowBtn)
+
+        component.vm.setArrowDisplay("right")
+
+        expect(component.vm.$data.arrowDisplay).toEqual("&#x2192")
+      })
+    })
+
+    describe("emitArrowBtnClick", () => {
+
+      it("should emit arrowClick", () => {
+        const component = shallowMount(ArrowBtn)
+
+        component.vm.emitArrowBtnClick()
+
+        const expected = { "arrowClick": [[]] }
+        expect(component.emitted()).toEqual(expected)
+      })
+    })
+  })
+
+  describe("user interactions", () => {
+
+    describe("events", () => {
+
+      it("should emit arrowClick when button is clicked", () => {
+        const component = shallowMount(ArrowBtn)
+
+        component.find(".arrow-btn").trigger("click")
+
+        const expected = { "arrowClick": [[]] }
+        expect(component.emitted()).toEqual(expected)
+      })
+    })
+  })
+})
