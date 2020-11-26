2f1e371a87fda264e5899ea48c4fba82a889ca4e
ZaneMeroff <zanemeroff@gmail.com>
2020-11-26 09:18:43 -0700

    Create SelectTeamPlayerNum route component

---
+++ src/views/SelectTeamPlayerNum.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+
+    <ArrowBtn
+      :arrowDirection="'left'"/>
+
+    <router-link class="router-link" to="/enterNames">
+      <ArrowBtn :arrowDirection="'right'"/>
+    </router-link>
+
+    <NumPickerIndex
+      :pickerType="'team'"/>
+
+    <NumPickerIndex
+      :pickerType="'player'"/>
+
+  </div>
+</template>
+
+<script>
+  // need spec with snapshot
+
+  import ArrowBtn from "../components/ArrowBtn"
+  import NumPickerIndex from "../components/NumPicker/NumPickerIndex"
+
+  export default {
+    name: "SelectTeamPlayerNum",
+    components: {
+      NumPickerIndex,
+      ArrowBtn,
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
