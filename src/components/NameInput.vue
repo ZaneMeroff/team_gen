de2b08be25f143d37e93edf695e37a852b2df142
ZaneMeroff <zanemeroff@gmail.com>
2020-11-26 11:06:44 -0700

    Create NameInput component

---
+++ src/components/NameInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <form autocomplete="off">
+
+    <input class="name-input" placeholder="name..."/>
+
+  </form>
+</template>
+
+<script>
+  export default {
+    name: "NameInput",
+    props: {},
+    components: {},
+    data() {
+      return {
+
+      }
+    },
+    methods: {},
+    computed: {},
+    watch: {},
+  }
+</script>
+
+<style scoped>
+
+  .name-input {
+    height: 30px;
+    width: 200px;
+    display: block;
+    border-radius: 5px;
+    margin: 10px 0px;
+    padding-left: 10px;
+  }
+
+</style>
