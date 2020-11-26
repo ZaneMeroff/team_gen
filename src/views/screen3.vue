ccaabe08560f9a8443ce292e15cef15b77e1428d
ZaneMeroff <zanemeroff@gmail.com>
2020-11-25 18:38:52 -0700

    Add router-link to screen3

--- src/views/screen3.vue
+++ src/views/screen3.vue
@@ -1,14 +1,22 @@
 <template>
+  <div>
 
-  <h1>screen 3</h1>
+    <router-link class="router-link" to="/screen2">
+      <ArrowBtn :arrowDirection="'left'"/>
+    </router-link>
 
+    <ArrowBtn :arrowDirection="'right'"/>
+
+  </div>
 </template>
 
 <script>
+  import ArrowBtn from "../components/ArrowBtn"
+
   export default {
     name: "Screen3",
     components: {
-
+      ArrowBtn,
     },
   }
 </script>
