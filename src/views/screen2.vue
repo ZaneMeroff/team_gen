e8aa9f217ff13f6bed0915f333e1e8b646cebc4e
ZaneMeroff <zanemeroff@gmail.com>
2020-11-25 17:37:20 -0700

    Create screen2

---
+++ src/views/screen2.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+
+    <router-link class="router-link" to="/">
+      <ArrowBtn :arrowDirection="'left'"/>
+    </router-link>
+
+    <ArrowBtn :arrowDirection="'right'"/>
+
+    <input style="display:block" placeholder="name 1"/>
+    <input style="display:block" placeholder="name 2"/>
+    <input style="display:block" placeholder="name 3"/>
+    <input style="display:block" placeholder="name 4"/>
+    <input style="display:block" placeholder="name 5"/>
+
+  </div>
+</template>
+
+<script>
+  import ArrowBtn from "../components/ArrowBtn"
+
+  export default {
+    name: "Screen2",
+    components: {
+      ArrowBtn,
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
