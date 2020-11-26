3fa4a7d9630795c286a615f9552abd7840b50fe7
ZaneMeroff <zanemeroff@gmail.com>
2020-11-26 09:17:59 -0700

    Create DisplayResults route component

---
+++ src/views/DisplayResults.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+
+    <router-link class="router-link" to="/enterNames">
+      <ArrowBtn :arrowDirection="'left'"/>
+    </router-link>
+
+    <ArrowBtn :arrowDirection="'right'"/>
+
+  </div>
+</template>
+
+<script>
+  // need spec with snapshot
+  
+  import ArrowBtn from "../components/ArrowBtn"
+
+  export default {
+    name: "DisplayResults",
+    components: {
+      ArrowBtn,
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
