10745cfcc922121823ee7c63357009c55d07f53f
ZaneMeroff <zanemeroff@gmail.com>
2020-11-29 10:28:59 -0700

    Create TeamCard component

---
+++ src/components/TeamCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="team-card-perimeter-container">
+
+    <p>team #{{ teamId }}</p>
+
+    <ul>
+      <li v-for="(member, index) in members" :key="index">{{ member }}</li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TeamCard",
+    props: {
+      members: { type: Array, default: [] },
+      teamId: { type: Number, default: 0 },
+    },
+    components: {},
+    data() {
+      return {
+
+      }
+    },
+    methods: {},
+    computed: {},
+    watch: {},
+  }
+</script>
+
+<style scoped>
+
+  .team-card-perimeter-container {
+    height: auto;
+    width: 300px;
+    border: 1px solid black;
+    margin: 10px;
+    border-radius: 10px;
+    padding-left: 10px;
+  }
+
+</style>
