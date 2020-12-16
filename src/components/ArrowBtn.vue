eabda057fbe42b1f6245ac49b79d827ad73b0089
ZaneMeroff <zanemeroff@gmail.com>
2020-12-16 12:43:06 -0700

    Alphabetize ArrowBtn.vue

--- src/components/ArrowBtn.vue
+++ src/components/ArrowBtn.vue
@@ -18,21 +18,21 @@
       return {
         arrowDisplay: "",
         arrowKey: {
-          up:    "&#8593",
           down:  "&#8595",
           left:  "&#x2190",
           right: "&#x2192",
+          up:    "&#8593",
         },
       }
     },
     methods: {
 
-      setArrowDisplay(direction) {
-        this.arrowDisplay = this.arrowKey[direction]
-      },
-
       emitArrowBtnClick() {
         this.$emit("arrowClick")
+      },
+
+      setArrowDisplay(direction) {
+        this.arrowDisplay = this.arrowKey[direction]
       },
     },
     mounted() {
@@ -44,39 +44,38 @@
 <style scoped>
 
   .arrow-btn {
-    height: 75px;
-    width: 75px;
-    display: flex;
-    justify-content: center;
     align-items: center;
-    font-size: 3rem;
-    border: none;
     background-color: transparent;
-  }
-
-  .arrow-btn:focus:not(:focus-visible) {
-    outline: none;
-  }
-
-  .nav-arrow-btn {
-    border-radius: 37.5px;
-    border: 1px solid #000;
-    background-image: linear-gradient(-180deg, #FF89D6 0%, #C01F9E 100%);
-    box-shadow: 0 1rem 1.25rem 0 rgba(22,75,195,0.50), 0 -0.25rem 1.5rem rgba(110, 15, 155, 1) inset;
-  }
-
-  .arrow-btn:hover {
-    cursor: pointer;
+    border: none;
+    display: flex;
+    font-size: 3rem;
+    height: 75px;
+    justify-content: center;
+    width: 75px;
   }
 
   .arrow-btn:active {
     color: #FFF;
   }
 
-  span {
-    font-family: sans-serif;
-    font-weight: bolder;
-    font-size: 3.6rem;
+  .arrow-btn:focus:not(:focus-visible) {
+    outline: none;
   }
 
+  .arrow-btn:hover {
+    cursor: pointer;
+  }
+
+  .nav-arrow-btn {
+    background-image: linear-gradient(-180deg, #FF89D6 0%, #C01F9E 100%);
+    border: 1px solid #000;
+    border-radius: 37.5px;
+    box-shadow: 0 1rem 1.25rem 0 rgba(22,75,195,0.50), 0 -0.25rem 1.5rem rgba(110, 15, 155, 1) inset;
+  }
+
+  span {
+    font-family: sans-serif;
+    font-size: 3.6rem;
+    font-weight: bolder;
+  }
 </style>
