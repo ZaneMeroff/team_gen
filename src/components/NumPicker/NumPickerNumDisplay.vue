eda45a9ef3cee61e64cc4c9d93ec494244b9f904
ZaneMeroff <zanemeroff@gmail.com>
2020-12-16 12:43:37 -0700

    Alphabetize NumPickerNumDisplay.vue

--- src/components/NumPicker/NumPickerNumDisplay.vue
+++ src/components/NumPicker/NumPickerNumDisplay.vue
@@ -12,26 +12,25 @@
   export default {
     name: "NumPickerNumDisplay",
     props: {
-      number:     { type: Number, default: 0 },
-      pickerType: { type: String, default: "teams" },
+      number:      { type: Number, default: 0 },
+      pickerType:  { type: String, default: "teams" },
     },
   }
 </script>
 
 <style scoped>
 
-  .npnd-perimeter-container {
-    height: 100%;
-    width: 50%;
-  }
-
   .npnd-num {
     font-size: 3.5rem;
     margin: 10px 0px 0px 30px;
   }
 
+  .npnd-perimeter-container {
+    height: 100%;
+    width: 50%;
+  }
+
   .picker-title {
     margin: 20px 0px 0px 20px;
   }
-
 </style>
